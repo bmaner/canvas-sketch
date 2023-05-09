de03dfa849ada0008c1d14bc86acfed834e30a22
bmaner <bmanerdaniel@gmail.com>
2023-05-09 22:05:13 +0900

    Add animation and connector between point

--- sketch-04.js
+++ sketch-04.js
@@ -1,5 +1,6 @@
 const canvasSketch = require("canvas-sketch");
 const random = require("canvas-sketch-util/random");
+const math = require("canvas-sketch-util/math");
 
 const settings = {
   dimensions: [1080, 1080],
@@ -20,6 +21,24 @@ const sketch = ({ context, width, height }) => {
     context.fillStyle = "white";
     context.fillRect(0, 0, width, height);
 
+    for (let i = 0; i < agents.length; i++) {
+      const agent = agents[i];
+
+      for (let j = i + 1; j < agents.length; j++) {
+        const other = agents[j];
+
+        const dist = agent.pos.getDistance(other.pos);
+        if (dist > 200) continue;
+
+        context.lineWidth = math.mapRange(dist, 0, 200, 12, 1);
+
+        context.beginPath();
+        context.moveTo(agent.pos.x, agent.pos.y);
+        context.lineTo(other.pos.x, other.pos.y);
+        context.stroke();
+      }
+    }
+
     agents.forEach((agent) => {
       agent.update();
       agent.draw(context);
@@ -34,6 +53,13 @@ class Vector {
   constructor(x, y) {
     this.x = x;
     this.y = y;
+  }
+
+  getDistance(v) {
+    const dx = this.x - v.x;
+    const dy = this.y - v.y;
+
+    return Math.sqrt(dx * dx + dy * dy);
   }
 }
 
@@ -68,3 +94,6 @@ class Agent {
     context.restore();
   }
 }
+
+//비디오 저장을 위한 명령어 canvas-sketch sketch-04.js --output=output/04 --stream
+//브라우저에서 command shift s로 녹화시작, command shift s로 녹화종료
