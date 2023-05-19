42abb47c62f6e880f5b7d9d76614780140d08d07
bmaner <bmanerdaniel@gmail.com>
2023-05-19 09:31:34 +0900

    Add color packages and fixed value of rectangles

--- sketch-08.js
+++ sketch-08.js
@@ -1,38 +1,92 @@
 const canvasSketch = require("canvas-sketch");
 const math = require("canvas-sketch-util/math");
+const random = require("canvas-sketch-util/random");
+const Color = require("canvas-sketch-util/color");
+const risoColors = require("riso-colors");
 
 const settings = {
   dimensions: [1080, 1080],
 };
 
-const sketch = () => {
-  let x, y, w, h;
-  let radius, angle, rx, ry;
+const sketch = ({ context, width, height }) => {
+  let x, y, w, h, fill, stroke, blend;
+
+  const num = 40;
+  const degrees = -30;
+
+  const rects = [];
+
+  const rectColors = [
+    random.pick(risoColors),
+    random.pick(risoColors),
+    random.pick(risoColors),
+  ];
+
+  const bgColor = random.pick(rectColors).hex;
+
+  for (let i = 0; i < num; i++) {
+    x = random.range(0, width);
+    y = random.range(0, height);
+    w = random.range(600, width);
+    h = random.range(40, 200);
+
+    // fill = `rgba(${random.range(0, 255)},${random.range(0, 255)},${random.range(
+    //   0,
+    //   255
+    // )},${random.range(0, 1)})`;
+    // stroke = "black";
+    fill = random.pick(rectColors).hex;
+    stroke = random.pick(rectColors).hex;
+
+    blend = random.value() > 0.5 ? "overlay" : "source-over";
+
+    rects.push({ x, y, w, h, fill, stroke });
+  }
+
   return ({ context, width, height }) => {
-    context.fillStyle = "white";
+    context.fillStyle = bgColor;
     context.fillRect(0, 0, width, height);
 
-    x = width * 0.5;
-    y = height * 0.5;
-    w = width * 0.6;
-    h = height * 0.1;
+    rects.forEach((rect) => {
+      const { x, y, w, h, fill, stroke, blend } = rect;
+      let shadowColor;
 
-    context.strokeStyle = "blue";
+      context.save();
+      context.translate(x, y);
+      context.strokeStyle = stroke;
+      context.fillStyle = fill;
+      context.lineWidth = 10;
 
-    context.save();
-    context.translate(x, y);
+      context.globalCompositionOperation = blend;
 
-    drawSkewedRect({ context });
-    context.stroke();
+      drawSkewedRect({ context, w, h, degrees });
 
-    context.restore();
+      shadowColor = Color.offsetHSL(fill, 0, 0, -20);
+      shadowColor.rgba[3] = 0.3;
+
+      context.shadowColor = Color.style(shadowColor.rgba);
+      context.shadowOffsetX = -10;
+      context.shadowOffsetY = 20;
+      context.fill();
+
+      context.shadowColor = null;
+      context.stroke();
+
+      context.globalCompositionOperation = blend;
+
+      context.lineWidth = 2;
+      context.strokeStyle = "black";
+      context.stroke();
+
+      context.restore();
+    });
   };
 };
 
 const drawSkewedRect = ({ context, w = 600, h = 200, degrees = -45 }) => {
-  angle = math.degToRad(degrees);
-  rx = Math.cos(angle) * w;
-  ry = Math.sin(angle) * w;
+  const angle = math.degToRad(degrees);
+  const rx = Math.cos(angle) * w;
+  const ry = Math.sin(angle) * w;
 
   context.save();
   context.translate(rx * -0.5, (rx + h) * -0.5);
@@ -44,8 +98,6 @@ const drawSkewedRect = ({ context, w = 600, h = 200, degrees = -45 }) => {
   context.lineTo(rx, ry + h);
   context.lineTo(0, h);
   context.closePath();
-  context.stroke();
-
   context.restore();
 };
 
